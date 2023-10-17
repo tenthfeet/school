35406a7f23d5cb7e16b6416e38ae8fc0cf7b2283
Ayyamperumal J <jperumal6@gmail.com>
2023-10-17 13:36:19 +0530

    sidebar scroll issue & validation msg issue fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,12 @@ import './bootstrap';
 
 // Import all of CoreUI's JS
 import * as coreui from '@coreui/coreui';
+import SimpleBar from 'simplebar';
+
+let sideNav = $('.sidebar-nav');
+if (sideNav.length > 0) {
+    new SimpleBar(sideNav[0]);
+}
 
 window.coreui = coreui;
 
